98f96ab3bbe29e82b34fec7a22055b014c790db3
jhermann <jh@web.de>
2015-11-11 17:02:40 +0100

    sync with new py-minimal-script template

--- setup.py
+++ setup.py
@@ -4,6 +4,18 @@
 """
     dependency-check - Shim to easily install OWASP dependency-check-cli into Python projects.
 
+    This setuptools script follows the DRY principle and tries to
+    minimize repetition of project metadata by loading it from other
+    places (like the script module's ``__*__`` attributes). Incidently,
+    this makes the script almost identical between different projects.
+
+    It is also importable (by using the usual ``if __name__ == '__main__'``
+    idiom), and exposes the project's setup data in a ``project`` dict.
+    This allows other tools to exploit the data assembling code contained
+    in here, and again supports the DRY principle. The ``rituals`` package
+    uses that to provide Invoke tasks that work for any project, based on
+    its project metadata.
+
     Copyright ©  2015 Jürgen Hermann <jh@web.de>
 
     Licensed under the Apache License, Version 2.0 (the "License");
@@ -18,15 +30,13 @@
     See the License for the specific language governing permissions and
     limitations under the License.
 """
-import io
-import os
-import re
-import sys
 
 project = dict(
     name='dependency-check',
     url='https://github.com/jhermann/dependency-check-py',
     license="Apache 2.0",
+    keywords='owasp, security, vulnerability, quality-assurance',
+    py_modules=['dependency_check'],
 
     install_requires=[],
 
@@ -34,7 +44,13 @@ project = dict(
         "console_scripts": [
             "dependency-check = dependency_check:run",
         ],
-    }
+    },
+
+    # Added by the code below:
+    #   version author author_email (from script module)
+    #   description (from doc string above)
+    #   long_description (from README)
+    #   classifiers (see below)
 )
 
 classifiers="""
@@ -58,14 +74,20 @@ Topic :: Software Development :: Quality Assurance
 Topic :: Utilities
 """
 
-# Import setuptools
+
+# Beyond this point, code is not project-specific
+import io
+import os
+import re
+import sys
+
 try:
     from setuptools import setup
 except ImportError as exc:
     raise RuntimeError("Cannot install '{0}', setuptools is missing ({1})".format(project['name'], exc))
 
 project_root = os.path.abspath(os.path.dirname(__file__))
-script_name = os.path.join(project_root, project['name'].replace('-', '_') + '.py')
+script_name = os.path.join(project_root, project['py_modules'][0] + '.py')
 expected_keys = "version author author_email".split()
 with io.open(script_name, encoding='utf-8') as handle:
     for line in handle:
@@ -74,10 +96,10 @@ with io.open(script_name, encoding='utf-8') as handle:
             project[match.group(1)] = match.group(3)
 
 project.update(dict(
-    py_modules=[project['name'].replace('-', '_')],
     description=__doc__.split('.')[0].split(' - ', 1)[1].strip(),
     long_description=io.open('README.rst', encoding='UTF-8').read().split('\n.. _setup-start:', 1)[-1].strip(),
     classifiers=[i.strip() for i in classifiers.splitlines() if i.strip() and not i.strip().startswith('#')],
+    keywords=project['keywords'].replace(',', ' ').strip().split(),
 ))
 
 # Ensure 'setup.py' is importable by other tools, to access the project's metadata
